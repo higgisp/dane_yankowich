9d09fedd218b2fc06d03effd32c0b430c0763bf6
Sam Higginbotham <higgisp@gmail.com>
2015-03-29 20:42:59 -0500

    Updated homepage

--- gulpfile.js
+++ gulpfile.js
@@ -51,7 +51,7 @@ gulp.task('scripts', function() {
 // Watch Files For Changes
 gulp.task('watch', function() {
   gulp.watch('js/*.js', ['lint', 'scripts']);
-  gulp.watch('scss/*.scss', ['sass']);
+  gulp.watch('scss/**/*.scss', ['sass']);
 });
 
 // Default Task
